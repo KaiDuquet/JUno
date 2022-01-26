34bbece68c9f6716e445e1d4cf57387431b3a88b
Kai Duquet <kaiduquet@gmail.com>
2022-01-26 16:32:48 -0500

    Cleaned up index page for future build

---
+++ public/index.js
@@ -0,0 +1,10 @@
+function onPlayClicked() {
+    const mainMenu = document.getElementById("main-menu");
+    const loginForm = document.getElementById("login-form");
+    mainMenu.setAttribute("style", "display: none;");
+    loginForm.removeAttribute("style");
+}
+
+function onHowToPlayClicked() {
+
+}
